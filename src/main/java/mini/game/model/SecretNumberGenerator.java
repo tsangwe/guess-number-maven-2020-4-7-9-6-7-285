abcc5d1f000105e754830701a8e2ebb766afe910
Wesley Tsang <tsangwe@oocl.com>
2020-04-08 18:36:39 +0800

    [Wesley] Create getNewSecretNumber and test

---
+++ src/main/java/mini/game/model/SecretNumberGenerator.java
@@ -0,0 +1,10 @@
+package mini.game.model;
+
+public class SecretNumberGenerator {
+    public SecretNumberGenerator() {}
+
+    public int[] getNewSecretNumber() {
+        
+        return null;
+    }
+}
---
+++ src/test/java/mini/game/model/SecretNumberGeneratorTest.java
@@ -0,0 +1,10 @@
+package mini.game.model;
+
+import org.junit.Test;
+
+public class SecretNumberGeneratorTest {
+    @Test
+    public void test_getNewSecretNumber() {
+        
+    }
+}
