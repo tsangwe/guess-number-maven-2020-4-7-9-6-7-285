f358934700cf839ba8e91a5ca4c83b9bf79134f1
Wesley Tsang <tsangwe@oocl.com>
2020-04-15 10:23:05 +0800

    [Wesley] Refactor extract constant

--- src/main/java/mini/game/model/Agent.java
+++ src/main/java/mini/game/model/Agent.java
@@ -1,33 +1,33 @@
 package mini.game.model;
 
+import mini.game.generator.SecretNumberGenerator;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.commons.lang3.math.NumberUtils;
-
-import mini.game.generator.SecretNumberGenerator;
-
 public class Agent {
+    private static final int NUMBER_LENGTH = 4;
     private static final String FEEDBACK_PATTERN = "%dA%dB";
-    private static final String WINNING_OUTPUT = "4A0B";
-
+    private static final String WINNING_OUTPUT = NUMBER_LENGTH + "A0B";
     private SecretNumberGenerator secretNumberGenerator;
+
 
     public Agent() {
         this.secretNumberGenerator = new SecretNumberGenerator();
     }
 
     public boolean isValidateInput(String input) {
-        if (input.length() != 4 || !NumberUtils.isParsable(input)) return false;
+        if (input.length() != NUMBER_LENGTH || !NumberUtils.isParsable(input)) return false;
 
         // Check for duplicates
-        Set<String> map = new HashSet<String>();
-        for (char numInChar: input.toCharArray()) {
+        Set<String> map = new HashSet<>();
+        for (char numInChar : input.toCharArray()) {
             String numCharInString = String.valueOf(numInChar);
             if (map.contains(numCharInString)) return false;
             map.add(numCharInString);
         }
-        
+
         return true;
     }
 
@@ -48,11 +48,10 @@ public class Agent {
             if (secretNumber[currIndex].equals(inputIntegerArray[currIndex])) {
                 countMatchCharAndPosition++;
                 countMatchOnlyChar++;
-                continue;
             } else {
                 for (int innerIdex = 0; innerIdex < secretNumber.length; innerIdex++) {
                     if (innerIdex == currIndex) continue;
-                    if (inputIntegerArray[currIndex] == secretNumber[innerIdex]) {
+                    if (inputIntegerArray[currIndex].equals(secretNumber[innerIdex])) {
                         countMatchOnlyChar++;
                         break;
                     }
@@ -65,8 +64,7 @@ public class Agent {
         return String.format(FEEDBACK_PATTERN, countMatchCharAndPosition, countMatchOnlyChar);
     }
 
-	public boolean isPlayerWin(String feedback) {
-        if (WINNING_OUTPUT.equals(feedback)) return true;
-		return false;
-	}
+    public boolean isPlayerWin(String feedback) {
+        return WINNING_OUTPUT.equals(feedback);
+    }
 }
