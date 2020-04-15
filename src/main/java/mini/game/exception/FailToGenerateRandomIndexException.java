47a62dddde571bc3c4efa361e5666d32583125fd
Wesley Tsang <tsangwe@oocl.com>
2020-04-15 10:14:23 +0800

    [Wesley] Refactor remove un-needed exception

--- src/main/java/mini/game/exception/FailToGenerateRandomIndexException.java
+++
@@ -1,9 +0,0 @@
-package mini.game.exception;
-
-public class FailToGenerateRandomIndexException extends Throwable {
-    private final static String errorMessage = "Cannot generate a random index.";
-
-    public FailToGenerateRandomIndexException() {
-        super(errorMessage);
-    }
-}
